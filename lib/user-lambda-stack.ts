003f83b10dcd9813fff2d29910cecef3906c1368
vernyuy <fonchu.e.venyuy@gmail.com>
2024-04-06 13:00:30 +0100

    getUserAccount

--- lib/user-lambda-stack.ts
+++ lib/user-lambda-stack.ts
@@ -26,6 +26,25 @@ export class UserLamdaStacks extends Stack {
       runtime: appsync.FunctionRuntime.JS_1_0_0,
     });
 
+    const getUserAccount = new appsync.AppsyncFunction(this, "getUserAccount", {
+      name: "getUserAccount",
+      api: acmsGraphqlApi,
+      dataSource: acmsDataSource,
+      code: bundleAppSyncResolver("src/resolvers/user/getUserAccount.ts"),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    new appsync.Resolver(this, "getUserAccountResolver", {
+      api: acmsGraphqlApi,
+      typeName: "Mutation",
+      fieldName: "getUserAccount",
+      code: appsync.Code.fromAsset(
+        join(__dirname, "./js_resolvers/_before_and_after_mapping_template.js")
+      ),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [getUserAccount],
+    });
+
     new appsync.Resolver(this, "createUserResolver", {
       api: acmsGraphqlApi,
       typeName: "Mutation",
