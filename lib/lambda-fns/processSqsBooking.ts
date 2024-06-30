f6b5da30b1cf265f409f5778f3b5ab43ae1cdec3
fonchu <fonchu.e.venyuy@gmail.com>
2024-06-30 07:34:33 +0100

    changed to aws v3

--- lib/lambda-fns/processSqsBooking.ts
+++ lib/lambda-fns/processSqsBooking.ts
@@ -1,55 +1,50 @@
-// import { Logger } from "@aws-lambda-powertools/logger";
-// import { DynamoDB } from "aws-sdk";
+import { Logger } from "@aws-lambda-powertools/logger";
+import { Context, SQSEvent, SQSRecord } from "aws-lambda";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 
-// import { Context, SQSEvent, SQSRecord } from "aws-lambda";
+const client = new DynamoDBClient();
+let tableName = process.env.ACMS_DB;
 
-// import { Tracer } from "@aws-lambda-powertools/tracer";
-// import { PutItemInputAttributeMap } from "aws-sdk/clients/dynamodb";
+const serviceName = "bookingHandler";
+const logger = new Logger({ logLevel: "INFO", serviceName: serviceName });
 
-// const namespace = "ApartmentComplexManagementApp";
-// const serviceName = "bookingHandler";
+exports.handler = async (event: SQSEvent, context: Context) => {
+  const failedMessageIds: string[] = [];
 
-// const logger = new Logger({ logLevel: "INFO", serviceName: serviceName });
-// const tracer = new Tracer({ serviceName: serviceName });
+  logger.addContext(context);
 
-// exports.handler = async (event: SQSEvent, context: Context) => {
-//   let tableName = process.env.ACMS_DB;
-//   const documentClient = new DynamoDB.DocumentClient();
-//   const failedMessageIds: string[] = [];
+  logger.info(`SQS events are ${JSON.stringify(event.Records)}`);
 
-//   logger.addContext(context);
+  const promises = event.Records.map(async (value: SQSRecord) => {
+    try {
+      const bookingDetails = JSON.parse(value.body);
+      if (tableName === undefined) {
+        logger.error(`Couldn't get the table name`);
+        tableName = "AcmsDynamoDBTable";
+      }
+      const params = {
+        TableName: tableName,
+        Item: bookingDetails,
+      };
+      const command = new PutItemCommand(params);
+      const response = await client.send(command);
 
-//   logger.info(`SQS events are ${JSON.stringify(event.Records)}`);
+      logger.info(`put parameters for booking is ${JSON.stringify(params)}`);
+    } catch (error) {
+      logger.error(
+        `an error occured during put booking ${JSON.stringify(error)}`
+      );
+      failedMessageIds.push(value.messageId);
+    }
+  });
+  // execute all promises
+  await Promise.all(promises);
 
-//   const promises = event.Records.map(async (value: SQSRecord) => {
-//     try {
-//       const bookingDetails: PutItemInputAttributeMap = JSON.parse(value.body);
-//       if (tableName === undefined) {
-//         logger.error(`Couldn't get the table name`);
-//         tableName = "AcmsDynamoDBTable";
-//       }
-//       const params = {
-//         TableName: tableName,
-//         Item: bookingDetails,
-//       };
-
-//       logger.info(`put parameters for booking is ${JSON.stringify(params)}`);
-//       await documentClient.put(params).promise();
-//     } catch (error) {
-//       logger.error(
-//         `an error occured during put booking ${JSON.stringify(error)}`
-//       );
-//       failedMessageIds.push(value.messageId);
-//     }
-//   });
-//   // execute all promises
-//   await Promise.all(promises);
-
-//   return {
-//     batchItemFailures: failedMessageIds.map((id) => {
-//       return {
-//         itemIdentifier: id,
-//       };
-//     }),
-//   };
-// };
+  return {
+    batchItemFailures: failedMessageIds.map((id) => {
+      return {
+        itemIdentifier: id,
+      };
+    }),
+  };
+};
